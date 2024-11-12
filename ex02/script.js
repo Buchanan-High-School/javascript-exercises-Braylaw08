82d7b45fd7921b54fb613e8461ede77686e2ffc3
b08red <600401@buchananschools.com>
2024-11-12 12:26:08 -0500

    I finished task 1 and started task 2

--- ex02/script.js
+++ ex02/script.js
@@ -1,11 +1,17 @@
 // Write your Task 1 code inside this function!
 function task1() {
   console.log("Running task 1");
+  let target = document.querySelector("#text-content")
+  console.log(target)
+  target.innerText = "something new"
 }
 
 // Write your task 2 code inside this function!
 function task2() {
   console.log("Running task 2");
+  let target = document.querySelector("#task2")
+  console.log(target)
+  
 }
 
 // Write your task 3 code inside this function!
