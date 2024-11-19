8b82ba49c06b3919c7567e0cf25caedfece746eb
b08red <600401@buchananschools.com>
2024-11-19 12:24:57 -0500

    Did task 1 and task 2b

--- ex04/script.js
+++ ex04/script.js
@@ -1,5 +1,6 @@
 function task1() {
-  let myArray = [];
+  let myArray = ["bread", "lettuce", "meat", "cheese", "ketchup", "bread"];
+  let target = document.querySelector('#task-1-target');
   // This will run when you click the button.
   myArray.forEach((item) => {
     let el = document.createElement("p");
@@ -10,10 +11,18 @@ function task1() {
 
 function task2A() {
   // Write your code in here
+  
 }
 
 function task2B() {
-  // Writ your code in here
+  // Write your code in here
+  let names = document.querySelectorAll("Li")
+let target = document.querySelector("#name-list")
+names.forEach(item => {
+  let newEl = document.createElement("li");
+  newEl.innerText = "Hi " +item.innerText;
+  target.appendChild(newEl)
+})
 }
 
 function task3() {
