ac253585d6ab42399a526016473e4944ee1eff9b
b08red <600401@buchananschools.com>
2025-01-07 12:03:25 -0500

    boiler plate for rps game

---
+++ ex02/logic/script.js
@@ -0,0 +1,13 @@
+let formInput = document.querySelector('#user-input');
+function rps(userInput) {
+    if(userInput == "rock") {
+        // stuff here
+    } else if(userInput == "paper") {
+        // stuff here
+    } else if(userInput == "scissors") {
+        // stuff here
+    } else {
+        // stuff here
+    }
+    
+}
